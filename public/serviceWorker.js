179f62f2861876e651475a8f5b96a4ec1cf5874f
Flavio Carvalho <flavio@vessi.com>
2024-08-28 00:15:42 -0700

    reset

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,3 +1,10 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "/";
+
 // Cache assets during the install step
 self.addEventListener('install', event => {
   event.waitUntil(
@@ -14,18 +21,58 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    self.clients.claim()
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
 // Fetch assets from cache
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      (async () => {
+        // Try to get the response from the preloadResponse promise
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        // Try to get the response from the cache
+        const cachedResponse = await caches.match(event.request);
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        // If not in cache, fetch from network
+        return fetch(event.request);
+      })()
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  }
 });
 
 self.addEventListener('message', function(event) {
   const data = event.data;
   console.log('Received message from main thread', data);
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: 'path-to-icon.png',
+  });
+});
+
+self.addEventListener('push', function(event) {
+  const data = event.data.json();
+  console.log('Received push event', data);
   self.registration.showNotification(data.title, {
     body: data.body,
     icon: 'path-to-icon.png',
--- src/App.js
+++ src/App.js
@@ -1,80 +1,157 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  ChakraProvider,
+  Container,
+  FormLabel,
+  Heading,
+  Stack,
+  StackDivider,
+  Switch,
+  Text,
+} from "@chakra-ui/react";
 
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/serviceWorker.js')
-      .then(function(registration) {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        new Notification('You will be notified when slots are available!');
-      }, function(error) {
-        console.log('ServiceWorker registration failed: ', error);
-      });
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceWorker.register("/serviceWorker.js").then(
+      function (registration) {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+        new Notification("You will be notified when slots are available!");
+      },
+      function (error) {
+        console.log("ServiceWorker registration failed: ", error);
+      }
+    );
   });
 }
-
 
 const App = () => {
   const [slots, setSlots] = useState([]);
   const [lastUpdate, setLastUpdate] = useState(null);
 
+  const [permission, setPermission] = useState(Notification.permission);
+
   useEffect(() => {
     const sendNotification = (slotCount, lastUpdate) => {
-      if (Notification.permission === 'granted') {
-        new Notification(`There are ${slotCount} available slots! At ${lastUpdate}`);
+      if (Notification.permission === "granted") {
+        new Notification(
+          `There are ${slotCount} available slots! At ${lastUpdate}`
+        );
       }
     };
-    
+
     const checkAvailableSlots = async () => {
       try {
-        const response = await fetch('https://ttp.cbp.dhs.gov/schedulerapi/slot-availability?locationId=5020');
+        const response = await fetch(
+          "https://ttp.cbp.dhs.gov/schedulerapi/slot-availability?locationId=5020"
+        );
         const data = await response.json();
+        const time = new Date().toLocaleTimeString();
 
-        setLastUpdate(new Date().toLocaleTimeString());
-  
+        setLastUpdate(time);
+
+        const dataWithTime = {
+          ...data,
+          fetchTime: time,
+        };
+
+        setSlots((prevSlots) => [dataWithTime, ...prevSlots]);
+
         if (data.availableSlots && data.availableSlots.length > 0) {
-          setSlots(data.availableSlots);
-          sendNotification(data.availableSlots.length, new Date().toLocaleTimeString());
-        } else {
-          setSlots([]);
+          sendNotification(
+            data.availableSlots.length,
+            new Date().toLocaleTimeString()
+          );
         }
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
-  
-    // Check for available slots every 30 seconds
-    const interval = setInterval(checkAvailableSlots, 15000);
-  
-    // Initial check
+
+    const interval = setInterval(checkAvailableSlots, 7000);
+
     checkAvailableSlots();
-  
+
     return () => clearInterval(interval);
-  }, []); // Empty dependency array to only run once on mount
-  
+  }, []);
 
   const requestNotificationPermission = () => {
-    if (Notification.permission === 'granted') {
-      new Notification('Notifications already enabled!');
+    if (Notification.permission === "granted") {
+      new Notification("Notifications already enabled!");
+      setPermission("granted");
     }
-    if (Notification.permission !== 'granted') {
+    if (Notification.permission !== "granted") {
+      setPermission("denied");
       Notification.requestPermission().then((permission) => {
-        if (permission === 'granted') {
-          new Notification('Notifications enabled! You');
+        if (permission === "granted") {
+          setPermission("granted");
+          new Notification("Notifications enabled! You");
         }
       });
     }
   };
 
   return (
-    <div className="App">
-      <h1>Nexus Slot Notifier</h1>
-      <button onClick={requestNotificationPermission}>
-        Enable Notifications
-      </button>
-      <p>{slots.length} slots available!</p>
-      <p>Last checked: {lastUpdate}</p>
-    </div>
+    <ChakraProvider>
+      {/* show service worker status */}
+      <Container p={6}>
+        <Card>
+          <CardHeader>
+            <Heading size="md">Service Worker Status</Heading>
+          </CardHeader>
+          <CardBody>
+            <Stack divider={<StackDivider />} spacing="4">
+              <Box>
+                <Text fontSize="sm">
+                  {navigator?.serviceWorker?.controller
+                    ? "Service Worker is active"
+                    : "Service Worker is not active"}
+                </Text>
+              </Box>
+            </Stack>
+          </CardBody>
+        </Card>
+        </Container>
+      <Container p={6}>
+        <Card>
+          <CardHeader>
+            <Heading size="md">
+              Nexus Slot Notifier - Last Updated at - {lastUpdate}
+            </Heading>
+          </CardHeader>
+          <CardBody>
+            <Stack divider={<StackDivider />} spacing="4">
+              <Box spacing={4}>
+                <FormLabel htmlFor="enableNotifications">
+                  Enable Notification:{" "}
+                  <Switch
+                    id="enableNotifications"
+                    isChecked={permission === "granted"}
+                    onChange={requestNotificationPermission}
+                  />
+                </FormLabel>
+              </Box>
+              <Box>
+                <Heading size="xs" textTransform="uppercase">
+                  Available Slots Listing
+                </Heading>
+                </Box>
+                {slots.slice(0, 6).map((slot, index) => (
+                  <Text pt="2" fontSize="sm" key={index}>
+                    {slot.availableSlots.length} Slots Available - Fetch at {slot.fetchTime}
+                  </Text>
+                ))}
+            </Stack>
+          </CardBody>
+        </Card>
+      </Container>
+    </ChakraProvider>
   );
 };
 
